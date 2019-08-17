ea203f9aba5ff072d1714352ed82cae41104fb8c
FPille <f.pille@gmail.com>
2019-08-17 13:39:21 +0200

    Run pytetst directly

--- test/test-appimage.sh
+++ test/test-appimage.sh
@@ -37,17 +37,17 @@ run_pytest()
 
     pushd squashfs-root
 
-    # HERE="$(dirname "$(readlink -f "${0}")")"
+     HERE="$(dirname "$(readlink -f "${0}")")"
 
-    # export APPDIR="$HERE"
-    # export PATH="$HERE/usr/bin:$HERE/usr/local/bin:$HERE/usr/python/bin:$PATH"
-    # export LD_PRELOAD="$HERE/usr/lib/liblept.so.5"
-    # export LD_LIBRARY_PATH="$HERE/usr/lib:$HERE/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-    # export TESSDATA_PREFIX="$HERE/usr/share/tesseract-ocr/4.00/tessdata"
-    # export GS_LIB="$HERE/usr/share/ghostscript/9.26/lib:$HERE/usr/share/ghostscript/9.26/Resource:$HERE/usr/share/ghostscript/9.26/Resource/Init"
+     export APPDIR="$HERE"
+     export PATH="$HERE/usr/bin:$HERE/usr/local/bin:$HERE/usr/python/bin:$PATH"
+     export LD_PRELOAD="$HERE/usr/lib/liblept.so.5"
+     export LD_LIBRARY_PATH="$HERE/usr/lib:$HERE/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+     export TESSDATA_PREFIX="$HERE/usr/share/tesseract-ocr/4.00/tessdata"
+     export GS_LIB="$HERE/usr/share/ghostscript/9.26/lib:$HERE/usr/share/ghostscript/9.26/Resource:$HERE/usr/share/ghostscript/9.26/Resource/Init"
 
     ./usr/python/bin/python3 -m pip install -r ../OCRmyPDF/requirements/test.txt
-    ./AppRun python3 -m pytest ../OCRmyPDF
+    ./usr/python/bin/python3 -m pytest ../OCRmyPDF
     popd
 }
 
