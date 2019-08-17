34afdbdd4d83f6fccc2808accbe5f73cd4cb1b8b
FPille <f.pille@gmail.com>
2019-08-17 14:59:02 +0200

    Add export file for test

--- test/test-appimage.sh
+++ test/test-appimage.sh
@@ -36,16 +36,8 @@ run_pytest()
     ./OCRmyPDF*.AppImage --appimage-extract > /dev/null 2>&1
 
     pushd squashfs-root
-
-     HERE="$(dirname "$(readlink -f "${0}")")"
-
-     export APPDIR="$HERE"
-     export PATH="$HERE/usr/bin:$HERE/usr/local/bin:$HERE/usr/python/bin:$PATH"
-     export LD_PRELOAD="$HERE/usr/lib/liblept.so.5"
-     export LD_LIBRARY_PATH="$HERE/usr/lib:$HERE/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-     export TESSDATA_PREFIX="$HERE/usr/share/tesseract-ocr/4.00/tessdata"
-     export GS_LIB="$HERE/usr/share/ghostscript/9.26/lib:$HERE/usr/share/ghostscript/9.26/Resource:$HERE/usr/share/ghostscript/9.26/Resource/Init"
-
+    cp "$TRAVIS_BUILD_DIR"/test/export.sh .
+    source export.sh
     ./usr/python/bin/python3 -m pip install -r ../OCRmyPDF/requirements/test.txt
     ./usr/python/bin/python3 -m pytest ../OCRmyPDF -n auto
     popd
