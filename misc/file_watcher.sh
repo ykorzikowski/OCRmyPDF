94f233eaee081268a8415dfe0d1f51006e9152ff
Yannik Korzikowski <yannik@korzikowski.de>
2019-09-20 13:34:37 +0200

    removing old pdf after ocr scan

--- misc/file_watcher.sh
+++ misc/file_watcher.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+do_ocr() {
+ /appenv/bin/ocrmypdf $1 $1_ocr.pdf
+
+ rm $1
+}
+
 while [ true ]; do
   sleep 5
-  find /pdf/ -iname '*.pdf' -type f -exec /appenv/bin/ocrmypdf {} {} \;
+  find /pdf/ -iname '*.pdf' -type f -exec do_ocr {} \;
 done
